a8af4258e5e1c69cd70b36d31d87615bc84d38ce
Damas <uint16@users.noreply.github.com>
2019-11-09 19:33:28 +0000

    iOS IDE test

---
+++ src/components/ProfileComponent.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Profile } from "../common/models/Profile";
+
+const ProfileApp = (_profile: Profile) => {
+
+    //retrieve profile photo using profileId
+    return(
+        <div>
+            <h2>Display Base Profile Here ...</h2>
+        </div>
+    )
+}
+
+export default ProfileApp;
