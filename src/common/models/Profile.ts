0ae71f0995d0ee901e8e5cdf73cfa310860083fa
Damas <didi@Mercury.local>
2020-03-31 11:32:51 +0100

    start over

--- src/common/models/Profile.ts
+++
@@ -1,8 +0,0 @@
-export interface Profile {
-    emptyImgUrl: string | undefined;
-    imgUrl: string | undefined;
-    name?: string,
-    dob?: Date,
-    party?: string,
-    constituency?: string
-}
