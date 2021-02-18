5771ea1f5471431554f6906892fd3ecbc549b2e2
Damas <uint16@users.noreply.github.com>
2021-02-18 21:46:09 +0000

    update landing page

--- ClientApp/src/components/visualizations/SearchComponent.tsx
+++ ClientApp/src/components/visualizations/SearchComponent.tsx
@@ -4,18 +4,30 @@ import './SearchComponent.css';
 
 const SearchComponent = () => {
   return (
+    <section className="landingSection">
+    <div>
+      <h1>Finding your next home has never been easier </h1>
     <Container className="searchContainer">
       <Form className="searchForm">
         <FormGroup >
-          <Label className="searchLabel" for="searchInput">I want to live here ...</Label>
-          <Input className="searchInput" type="text" name="email" id="searchInput" placeholder="e.g Dodoma, Kinondoni" />
+          <Label className="searchLabel" for="searchInput">Start by telling us where you want to live ...</Label>
+          <Input className="searchInput" type="text" name="location" id="searchInput" placeholder="e.g Dodoma, Kinondoni" />
         </FormGroup>
         <div className="buttonsContainer">
+        <div>
         <Button className="searchButton">Buy</Button>
+        </div>
+        <div>
         <Button className="searchButton">Rent</Button>
+        </div>
         </div>
       </Form>
     </Container>
+    </div>
+    {/* <div className="landingImageDiv">
+      <img src="http://placeimg.com/640/480/arch"/>
+    </div> */}
+    </section>
   )
 }
 
